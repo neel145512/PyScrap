8dc3375c7df1408c23d144ee6074b5b9b5e1feb9
neel145512 <60291226+neel145512@users.noreply.github.com>
2021-11-21 13:35:20 -0500

    Add files via upload

---
+++ Images/Icons/img.py
@@ -0,0 +1,17 @@
+from PIL import Image
+
+filename = 'delete.png'
+
+img = Image.open(filename)
+img = img.convert('RGBA')
+datas = img.getdata()
+
+newData = []
+for item in datas:
+    if item[0] == 255 and item[1] == 255 and item[2] == 255:
+        newData.append((255, 255, 255, 0))
+    else:
+        newData.append(item)
+
+img.putdata(newData)
+img.save(filename, "PNG")
